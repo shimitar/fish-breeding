9593c3ec6203c56f4fb5bc6a6285739efbdcb26d
shimitar <8bi2sz6723@gmail.com>
2022-10-19 21:47:17 +0900

    新規登録・ログイン機能

--- app/controllers/fishes_controller.rb
+++ app/controllers/fishes_controller.rb
@@ -3,6 +3,10 @@ class FishesController < ApplicationController
     @fishes = Fish.includes(:user)
   end
 
+  def new
+   @fishes = Fish.new
+   end
+
 private
   def fish_params
     params.require(:fish).permit(:name, :size, :fish_text, :category_id, :image).merge(user_id: current_user.id)
