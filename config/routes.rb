aec05867a5de3ac528bb2e0c66b23e5d20b59366
shimitar <8bi2sz6723@gmail.com>
2022-10-24 12:16:47 +0900

    ビューの作成、編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "fishes#index"
 
-  resources :fishes, only: [:index, :new, :create, :show, :edit, :update]
+  resources :fishes, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :breeds, only: [:update]
+  end
 end
