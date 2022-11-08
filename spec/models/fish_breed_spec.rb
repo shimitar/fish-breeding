31e46a6da63ec94d137e16c7c73c54bbdd8b5fcc
shimitar <8bi2sz6723@gmail.com>
2022-11-08 16:15:20 +0900

    formオブジェクトを使ったfish_breedモデルのエラーメッセージを除いたテストコードの記述

--- spec/models/fish_breed_spec.rb
+++ spec/models/fish_breed_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe FishBreed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '魚の新規投稿' do
+    before do
+      user = FactoryBot.create(:user)
+      fish = FactoryBot.create(:fish)
+      @fish_breed = FactoryBot.build(:fish_breed, fish_id: fish.id, user_id: user.id)
+      sleep(1)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@fish_breed).to be_valid
+      end
+      it '水槽の大きさは空でも保存できる' do
+        @fish_breed.tank_size = ''
+        expect(@fish_breed).to be_valid
+      end
+      it '水温は空でも保存できる' do
+        @fish_breed.temperature = ''
+        expect(@fish_breed).to be_valid
+      end
+      it '使っているフィルターは空でも保存できる' do
+        @fish_breed.filter = ''
+        expect(@fish_breed).to be_valid
+      end
+      it '使っているライトは空でも保存できる' do
+        @fish_breed.raito = ''
+        expect(@fish_breed).to be_valid
+      end
+      it '水質は空でも保存できる' do
+        @fish_breed.condition = ''
+        expect(@fish_breed).to be_valid
+      end
+      it '底砂は空でも保存できる' do
+        @fish_breed.sand = ''
+        expect(@fish_breed).to be_valid
+      end
+      it '飼育環境は空でも保存できる' do
+        @fish_breed.breeding_text = ''
+        expect(@fish_breed).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it '魚の名前が空だと保存できない' do
+        @fish_breed.name = ''
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+      end
+      it '魚の大きさが空だと保存できない' do
+        @fish_breed.size = ''
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+      end
+      it '魚の大きさにcmかmを入力しないと保存できない' do
+        @fish_breed.size = '10'
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+      end
+      it '魚の大きさが全角数字だと保存できない' do
+        @fish_breed.size = '１０cm'
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+      end
+      it '魚のカテゴリーを選択していないと保存できない' do
+        @fish_breed.category_id = 1
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+      end
+      it '魚の説明が空だと保存できない' do
+        @fish_breed.fish_text = ''
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+      end
+      it '魚の情報が紐付いていないと保存できない' do
+        @fish_breed.fish_id = nil
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("商品を入力してください")
+      end
+      it 'ユーザーが紐付いていないと保存できない' do
+        @fish_breed.user_id = nil
+        @fish_breed.valid?
+        expect(@fish_breed.errors.full_messages).to include("ユーザーを入力してください")
+      end
+    end
+  end
 end
