5a7c46ad4d901e7d2c4defab2cebe54382f64964
shimitar <8bi2sz6723@gmail.com>
2022-11-15 10:54:05 +0900

    render-build.shの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle install
+bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
