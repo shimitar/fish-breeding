1fec0d1542dafb1f80ac4fbeabf81eefcb5b19d9
shimitar <8bi2sz6723@gmail.com>
2022-11-23 21:52:26 +0900

    ユーザー新規登録できる時のテスト実装

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+    # トップページに移動する
+    visit root_path
+    # トップページにサインアップページへ遷移するボタンがあることを確認する
+    expect(page).to have_content('新規登録')
+    # 新規登録ページへ移動する
+    visit new_user_registration_path
+    # ユーザー情報を入力する
+    fill_in "user[email]", with: @user.email
+    fill_in "user[password]", with: @user.password
+    fill_in "user[password_confirmation]", with: @user.password_confirmation
+    fill_in "user[nickname]", with: @user.nickname
+    # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+    expect{
+      find('input[name="commit"]').click
+    }.to change { User.count }.by(1)
+    # トップページへ遷移したことを確認する
+    expect(current_path).to eq(root_path)
+    # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+    expect(page).to have_no_content('新規登録')
+    expect(page).to have_no_content('ログイン')
+  end
+end
