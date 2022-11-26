6b45f42fdc9e82f0b35e8e2bc167b947a4e6359f
shimitar <8bi2sz6723@gmail.com>
2022-11-26 21:27:04 +0900

    ユーザー新規登録出来ないときのテスト実

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -27,4 +27,24 @@ RSpec.describe "ユーザー新規登録", type: :system do
     expect(page).to have_no_content('新規登録')
     expect(page).to have_no_content('ログイン')
   end
+
+  it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+    # トップページに移動する
+    visit root_path
+    # トップページにサインアップページへ遷移するボタンがあることを確認する
+    expect(page).to have_content('新規登録')
+    # 新規登録ページへ移動する
+    visit new_user_registration_path
+    # ユーザー情報を入力する
+    fill_in "user[email]", with: ''
+    fill_in "user[password]", with: ''
+    fill_in "user[password_confirmation]", with: ''
+    fill_in "user[nickname]", with: ''
+    # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+    expect{
+      find('input[name="commit"]').click
+    }.to change { User.count }.by(0)
+    # 新規登録ページへ戻されることを確認する
+    expect(current_path).to eq user_registration_path
+  end
 end
